2a4bf905d816413f3ab8c5e7d9c47fd7d27837cd
Wyatt Childers <wchilders@nearce.com>
2020-01-30 19:29:39 -0500

    Only use plains biomes

--- src/main/java/gg/packetloss/voidgen/VoidGenTerrainGenerator.java
+++ src/main/java/gg/packetloss/voidgen/VoidGenTerrainGenerator.java
@@ -1,6 +1,7 @@
 package gg.packetloss.voidgen;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.block.Biome;
 import org.bukkit.generator.BlockPopulator;
 import org.bukkit.generator.ChunkGenerator;
 
@@ -26,6 +27,12 @@ public class VoidGenTerrainGenerator extends ChunkGenerator {
 
     @Override
     public ChunkData generateChunkData(World world, Random random, int chunkx, int chunkz, BiomeGrid biome) {
+        for (int x = 0; x < 16; ++x) {
+            for (int z = 0; z < 16; ++z) {
+                biome.setBiome(x, z, Biome.PLAINS);
+            }
+        }
+
         return createChunkData(world);
     }
 }
